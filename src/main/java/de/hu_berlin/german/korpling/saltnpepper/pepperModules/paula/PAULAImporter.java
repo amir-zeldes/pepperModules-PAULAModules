58c760c68c362c1b2448df0d53db14d7011f021f
FlorianZipser <f.zipser@gmx.de>
2013-04-23 14:03:26 +0000

    removed outcommented lines of code
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/PAULAModules/branches/newImportCorpusStructure@7862 47085195-c50e-4317-8f43-7cb808db0f00

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/PAULAImporter.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/PAULAImporter.java
@@ -19,8 +19,6 @@ package de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Hashtable;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,19 +29,15 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
-import org.osgi.service.log.LogService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.log.LogService;
 
 import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperExceptions.PepperFWException;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperExceptions.PepperModuleException;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperImporter;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.impl.PepperImporterImpl;
 import de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula.exceptions.PAULAImporterException;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.SaltCommonFactory;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpus;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusDocumentRelation;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusRelation;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SElementId;
 
@@ -150,146 +144,12 @@ public class PAULAImporter extends PepperImporterImpl implements PepperImporter
 	 * a property representation of a property file
 	 */
 	protected Properties props= null;
-	
-//	/**
-//	 * Stores relation between documents and their resource 
-//	 */
-//	private Map<SElementId, URI> sDocumentResourceTable= null;
-//	
-//	private SCorpusGraph sCorpusGraph= null;
-//	
-//	/**
-//	 * This method is called by Pepper at the start of conversion process. 
-//	 * It shall create the structure the corpus to import. That means creating all necessary SCorpus, 
-//	 * SDocument and all Relation-objects between them. The path tp the corpus to import is given by
-//	 * this.getCorpusDefinition().getCorpusPath().
-//	 * @param an empty graph given by Pepper, which shall contains the corpus structure
-//	 */
-//	@Override
-//	public void importCorpusStructure(SCorpusGraph sCorpusGraph)
-//			throws PepperModuleException 
-//	{
-//		if (this.getCorpusDefinition().getCorpusPath()== null)
-//			throw new PAULAImporterException("Cannot import corpus-structure, because no corpus-path is given.");
-//		timeImportSCorpusStructure= System.nanoTime();
-//		
-//		this.sDocumentResourceTable= new Hashtable<SElementId, URI>();	
-//		this.sCorpusGraph= sCorpusGraph;
-//		File corpusPath= new File(this.getCorpusDefinition().getCorpusPath().toFileString());
-//		this.extractCorpusStructureRec(corpusPath, corpusPath.getName(), null);
-//		timeImportSCorpusStructure= System.nanoTime()- timeImportSCorpusStructure;
-//	}
+
 //// ========================== start: extract corpus-path	
 	/**
 	 * Stores the endings which are used for paula-files
 	 */
 	private String[] PAULA_FILE_ENDINGS= {"xml","paula"};
-//	
-//	/**
-//	 * Reads the file structure of the given corpus directory and translates it to a corpus-structure.
-//	 * @param corpusPath
-//	 * @param currentPath the string representation of path which will be used to create the SElementId
-//	 */
-//	private void extractCorpusStructureRec(File corpusPath, String currentPath, SCorpus parentSCorpus)
-//	{
-//		if (!corpusPath.exists())
-//			throw new PAULAImporterException("Cannot import corpus-structure, because the corpus-path does not exist: "+ corpusPath.getAbsolutePath());
-//		Boolean hasPAULAFiles= false;
-//		Boolean hasFolders= false;
-//		EList<File> subFolders= new BasicEList<File>();
-//		for (File currentFile: corpusPath.listFiles())
-//		{
-//			if (currentFile.isDirectory())
-//			{
-//				hasFolders= true;
-//				subFolders.add(currentFile);
-//			}
-//			else if(currentFile.isFile())
-//			{
-//				String currentEnding= null;
-//				String[] parts= currentFile.getName().split("[.]");
-//				if (parts.length>=2)
-//				{
-//					currentEnding= parts[parts.length-1];
-//				}
-//				for (String PAULAEnding: PAULA_FILE_ENDINGS)
-//				{
-//					if (currentEnding.equalsIgnoreCase(PAULAEnding))
-//					{
-//						hasPAULAFiles= true;
-//						break;
-//					}
-//				}
-//			}
-//		}
-//		if (	(!hasFolders) &&
-//				(!hasPAULAFiles))
-//		{//folder doesn't contain PAULA-files or other folders
-//			if (this.getLogService()!= null)
-//				this.getLogService().log(LogService.LOG_WARNING, "The folder '"+corpusPath.getAbsolutePath()+"' will not be imported, because it does not contain other folders or PAULA-documents.");
-//		}//folder doesn't contain PAULA-files or other folders
-//		else if (	(hasPAULAFiles) &&
-//					(!hasFolders))
-//		{//the current folder is a document
-//			if (parentSCorpus== null)
-//			{//create an artificial root corpus, because no one exists in data
-//				SElementId sCorpusId= SaltCommonFactory.eINSTANCE.createSElementId();
-//				sCorpusId.setSId(currentPath);
-//				parentSCorpus= SaltCommonFactory.eINSTANCE.createSCorpus();
-//				parentSCorpus.setSName(corpusPath.getName());
-//				parentSCorpus.setSElementId(sCorpusId);
-//				this.sCorpusGraph.addSNode(parentSCorpus);
-//				if (this.getLogService()!= null)
-//					this.getLogService().log(LogService.LOG_WARNING, "No root corpus exists in PAULA data (in '"+corpusPath.getAbsolutePath()+"'), an artificial one with the name '"+corpusPath.getName()+"' has been created.");
-//			}//create an artificial root corpus, because no one exists in data
-//			SElementId sDocumentId= SaltCommonFactory.eINSTANCE.createSElementId();
-//			sDocumentId.setSId(currentPath);
-//			String documentName= corpusPath.getName();
-//			SDocument sDocument= SaltCommonFactory.eINSTANCE.createSDocument();
-//			sDocument.setSName(documentName);
-//			sDocument.setSElementId(sDocumentId);
-//			this.sCorpusGraph.addSNode(sDocument);
-//			SCorpusDocumentRelation sCorpDocRel= SaltCommonFactory.eINSTANCE.createSCorpusDocumentRelation();
-//			sCorpDocRel.setSDocument(sDocument);
-//			sCorpDocRel.setSCorpus(parentSCorpus);
-//			this.sCorpusGraph.addSRelation(sCorpDocRel);
-//			
-//			URI sDocumentURI= URI.createFileURI(corpusPath.getAbsolutePath());
-//			this.sDocumentResourceTable.put(sDocumentId, sDocumentURI);
-//		}//the current folder is a document
-//		else if (hasFolders)
-//		{//the current folder is a corpus
-//			if(	(hasPAULAFiles) &&
-//				(hasFolders))
-//			{//folder does also contain PAULA-files and other folders --> ignore the files 
-//				if (this.getLogService()!= null)
-//					this.getLogService().log(LogService.LOG_WARNING, "The folder '"+corpusPath.getAbsolutePath()+"' also contains sub folders and PAULA-documents. This is not compatible to the PAULA-format. Therefore the PAULA-files will be ignored.");
-//			}//folder does also contain PAULA-files and other folders --> ignore the files
-//			
-//			SElementId sCorpusId= SaltCommonFactory.eINSTANCE.createSElementId();
-//			sCorpusId.setSId(currentPath);
-//			SCorpus sCorpus= SaltCommonFactory.eINSTANCE.createSCorpus();
-//			sCorpus.setSName(corpusPath.getName());
-//			sCorpus.setSElementId(sCorpusId);
-//			this.sCorpusGraph.addSNode(sCorpus);
-//			if (parentSCorpus!= null)
-//			{//if parent-corpus exists create a relation between parent and current	
-//				SCorpusRelation sCorpRel= SaltCommonFactory.eINSTANCE.createSCorpusRelation();
-//				sCorpRel.setSSuperCorpus(parentSCorpus);
-//				sCorpRel.setSSubCorpus(sCorpus);			
-//				this.sCorpusGraph.addSRelation(sCorpRel);
-//			}//if parent-corpus exists create a relation between parent and current
-//			if (subFolders!= null)
-//			{	
-//				for (File subFolder: subFolders)
-//				{
-//					StringBuilder newCorpusPath= new StringBuilder();
-//					newCorpusPath.append((currentPath== null)? subFolder.getName().trim() : currentPath+"/"+subFolder.getName().trim()); 
-//					this.extractCorpusStructureRec(subFolder, newCorpusPath.toString(), sCorpus);
-//				}
-//			}
-//		}//the current folder is a corpus
-//	}
 // ========================== end: extract corpus-path
 	/**
 	 * Extracts properties out of given special parameters.
